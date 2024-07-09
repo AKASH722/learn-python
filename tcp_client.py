242137580531395a822528685aa65ae7c6f3e5b7
AKASH RAI <akashrai4546@gmail.com>
2024-07-09 13:29:08 +0530

    tcp

---
+++ tcp_client.py
@@ -0,0 +1,12 @@
+import socket
+# host = socket.gethostname() #as both run on same computer
+host = '192.168.93.138'
+port = 5000
+client_socket = socket.socket()
+client_socket.connect((host, port))
+msg = input("client: ")
+while msg.lower().strip() != "bye":
+    client_socket.send(msg.encode())
+    print("server:", client_socket.recv(1024).decode())
+    msg = input("client: ")
+client_socket.close()
