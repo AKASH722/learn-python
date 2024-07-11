bdccfa24a2161b1465447e6b6dc4ebd41a4a7226
AKASH RAI <akashrai4546@gmail.com>
2024-07-11 10:40:53 +0530

    udp, simple browser, server

---
+++ Web Programming Using Python/udp/udp_client.py
@@ -0,0 +1,14 @@
+import socket
+host=socket.gethostname()
+port=8000
+ADDR=host, port
+client=socket.socket(type=socket.SOCK_DGRAM)
+while True:
+    msg=input('client --> ')
+    client.sendto(msg.encode(), ADDR)
+    if not msg: break
+    rep, ADDR=client.recvfrom(1024)
+    rep=rep.decode()
+    if not rep: break
+    print(f'server({ADDR}) --> {rep}')
+client.close()
---
+++ Web Programming Using Python/udp/udp_server.py
@@ -0,0 +1,14 @@
+import socket
+host=socket.gethostname()
+port=8000
+ADDR=host, port
+server=socket.socket(type=socket.SOCK_DGRAM)
+server.bind(ADDR)
+while True:
+    rep, addr=server.recvfrom(1024)
+    if not rep: break
+    print(f'client({addr}) --> {rep.decode()}')
+    msg=input('server --> ')
+    server.sendto(msg.encode(), addr)
+    if not msg: break
+server.close()
